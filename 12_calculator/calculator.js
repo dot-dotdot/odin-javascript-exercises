739ca12195e1afe62e9660ad3a3275715a713872
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-08-05 22:19:48 +0700

    Solve exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let product = 1;
+  for (let i = 0; i < exponent; i++) {
+    product *= base;
+  }
+  
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let product = 1;
+  for (let i = 2; i <= number; i++) {
+    product *= i;
+  } 
+
+  return product;
 };
 
 // Do not edit below this line
