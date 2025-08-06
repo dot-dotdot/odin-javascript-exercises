6f89ef1089825673e3afa20b977905c9f8cb6d75
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-08-06 13:15:43 +0700

    Solve exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fibNumberIndex) {
+    if (fibNumberIndex >= 0) {
+        let i = 1;
+        let fib1 = 0;
+        let fib2 = 1;
 
+        while (i <= fibNumberIndex) {
+            let nextFibNumber = fib2 + fib1;
+            fib1 = fib2;
+            fib2 = nextFibNumber;
+
+            i++;
+        }
+
+        return fib1;
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
