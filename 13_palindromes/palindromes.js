ae0f5ada93368c956035dda216b595c247877d34
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-08-06 12:57:35 +0700

    Solve exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = string
+                        .toLowerCase()
+                        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                        .replace(/\s+/g, "");
 
+    let leftIndex = 0;
+    let rightIndex = cleanString.length - 1;
+
+    while (leftIndex < rightIndex) {
+        let leftChar = cleanString.charAt(leftIndex);
+        let rightChar = cleanString.charAt(rightIndex);
+
+        if (leftChar !== rightChar) {
+            return false;
+        }
+
+        leftIndex++;
+        rightIndex--;
+    }
+
+    return true;
 };
+
+palindromes("Animal loots foliated detail of stool lamina.");
 
 // Do not edit below this line
 module.exports = palindromes;
